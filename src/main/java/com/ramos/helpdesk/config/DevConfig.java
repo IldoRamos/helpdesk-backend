0e0d303e742b4bbe2abe699bbe5c03aa7f1da878
IldoRamos <ildoramosvieira@gmail.com>
2024-07-12 00:18:56 -0300

    Perfil de desenvolvedor

--- src/main/java/com/ramos/helpdesk/config/DevConfig.java
+++ src/main/java/com/ramos/helpdesk/config/DevConfig.java
@@ -1,6 +1,7 @@
 package com.ramos.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -9,15 +10,19 @@ import com.ramos.helpdesk.service.DBService;
 import jakarta.annotation.PostConstruct;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
 	@PostConstruct
     public void instanciaDB() {
+		if(value.equals("create")) {
 			this.dbService.instaciaDB();
+		}
 	}
-	
 }
